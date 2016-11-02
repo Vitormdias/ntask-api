fbc7f781949ad46de9919f4462ce59f0131bfddc
Vitor <vitormd97@gmail.com>
2016-11-02 12:20:49 -0200

    Routes added

---
+++ routes/tasks.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+    app.get("/", (req, res) => res.json({status: "NTask API"}));
+}
+
+module.exports = app => {
+  const Tasks = app.models.tasks;
+  app.get("/tasks", (req, res) => {
+    Tasks.findAll({}, (tasks) => {
+      res.json({tasks: tasks});
+    });
+  });
+}
