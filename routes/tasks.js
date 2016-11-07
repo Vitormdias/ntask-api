8b5ab51749feb22b5e9ca1fd8b3fcd7d31e81fe9
vitormdias <vitormd97@gmail.com>
2016-11-07 17:04:29 -0200

    Model Tasks updated

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,8 @@
 module.exports = app => {
-    app.get("/", (req, res) => res.json({status: "NTask API"}));
-}
-
-module.exports = app => {
-  const Tasks = app.models.tasks;
-  app.get("/tasks", (req, res) => {
-    Tasks.findAll({}, (tasks) => {
+  const Tasks = app.db.models.Tasks;
+  app.get('/tasks', (req, res) => {
+    Tasks.findAll({}).then(tasks => {
       res.json({tasks: tasks});
     });
   });
-}
+};
