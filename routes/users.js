09061bd6d7af99ddd5b62d58b71e6dc76cc5914b
vitormdias <vitormd97@gmail.com>
2016-11-16 17:31:02 -0200

    Authentication added in users routes

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,37 @@
 module.exports = app => {
     const Users = app.db.models.Users;
+
     app.get("/users/:id", (req, res) => {
         Users.findById(req.params.id, {
             attributes: ["id", "name", "email"]
         })
         .then(result => res.json(result))
         .catch(error => {
-            res.status(412).json({msg: error.message});
+            res.status(412).json({
+                msg: error.message
+            });
         });
     });
     app.delete("/users/:id", (req, res) => {
-        Users.destroy( {
+        Users.destroy({
             where: {
-                id: req.params.id
+                id: req.user.id
             }
         })
         .then(result => res.sendStatus(204))
         .catch(error => {
-            res.status(412).json({msg: error.message});
+            res.status(412).json({
+                msg: error.message
+            });
         });
     });
     app.post("/users", (req, res) => {
         Users.create(req.body)
         .then(result => res.json(result))
         .catch(error => {
-            res.status(412).json({msg: error.message});
+            res.status(412).json({
+                msg: error.message
+            });
         });
     });
 };
