ec06b116e29088e2ec03f3dd7f4828600b5783b9
vitormdias <vitormd97@gmail.com>
2016-11-16 17:30:08 -0200

    Authentication on tasks routes implemented

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,48 +1,76 @@
 module.exports = app => {
   const Tasks = app.db.models.Tasks;
 
-  app.route('/tasks')
-    .get((req,res) => {
-      Tasks.findAll({})
-        .then(result => res.json(result))
-        .catch(error => {
-          res.status(412).json({msg: error.message});
+    app.route("/tasks")
+    .all(app.auth.authenticate())
+    .get((req, res) => {
+      Tasks.findAll({
+        where: {
+          user_id: req.user.id
+        }
+      })
+      .then(result => res.json(result))
+      .catch(error => {
+        res.status(412).json({
+          msg: error.message
         });
+      });
     })
-    .post((req,res) => {
+    .post((req, res) => {
+      req.body.user_id = req.user.id;
       Tasks.create(req.body)
-        .then(result => res.json(result))
-        .catch(error => {
-          res.status(412).json({msg: error.message});
+      .then(result => res.json(result))
+      .catch(error => {
+        res.status(412).json({
+          msg: error.message
         });
+      });
     });
 
-    app.route('/tasks/:id')
-      .get((req,res) => {
-        Tasks.findOne({where: req.params})
-          .then(result => {
-            if(result) {
-              res.json(result);
-            } else {
-              res.sendStatus(404);
+    app.route("/tasks/:id")
+    .all(app.auth.authenticate())
+    .get((req, res) => {
+        Tasks.findOne({
+          where: {
+            id: req.params.id,
+            user_id: req.user.id
+        }})
+        .then(result => {
+            if (result) {
+                return res.json(result);
             }
-          })
-          .catch(error => {
-            res.status(412).json({msg: error.message});
-          });
-      })
-      .put((req,res) => {
-        Tasks.update(req.body, {where: req.params})
-          .then(result => res.sendStatus(204))
-          .catch(error => {
-            res.status(412).json({msg: error.message});
-          });
-      })
-      .delete((req,res) => {
-        Tasks.destroy({where: req.params})
-          .then(result => res.sendStatus(204))
-          .catch(error => {
-            res.status(412).json({msg: error.message});
-          });
-      });
-};
+            return res.sendStatus(404);
+        })
+        .catch(error => {
+            res.status(412).json({
+              msg: error.message
+            });
+        });
+    })
+    .put((req, res) => {
+        Tasks.update(req.body, {
+          where: {
+            id: req.params.id,
+            user_id: req.user.id
+        }})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+            res.status(412).json({
+              msg: error.message
+            });
+        });
+    })
+    .delete((req, res) => {
+        Tasks.destroy({
+          where: {
+            id: req.params.id,
+            user_id: req.user.id
+        }})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+            res.status(412).json({
+              msg: error.message
+            });
+        });
+    });
+}
