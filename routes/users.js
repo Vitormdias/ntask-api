0ddef324e91ef68e2f13c201ad2edfbb2eaf9e34
vitormdias <vitormd97@gmail.com>
2016-11-16 21:31:29 -0200

    users route refactoring

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,18 @@
 module.exports = app => {
     const Users = app.db.models.Users;
 
+    function errorHandling(error) {
+        return {
+            msg: error.message
+        }
+    }
+
     app.get("/users/:id", (req, res) => {
         Users.findById(req.params.id, {
             attributes: ["id", "name", "email"]
         })
         .then(result => res.json(result))
-        .catch(error => {
-            res.status(412).json({
-                msg: error.message
-            });
-        });
+        .catch(error => res.status(412).json(errorHandling(error)));
     });
     app.delete("/users/:id", (req, res) => {
         Users.destroy({
@@ -19,19 +21,11 @@ module.exports = app => {
             }
         })
         .then(result => res.sendStatus(204))
-        .catch(error => {
-            res.status(412).json({
-                msg: error.message
-            });
-        });
+        .catch(error => res.status(412).json(errorHandling(error)));
     });
     app.post("/users", (req, res) => {
         Users.create(req.body)
         .then(result => res.json(result))
-        .catch(error => {
-            res.status(412).json({
-                msg: error.message
-            });
-        });
+        .catch(error => res.status(412).json(errorHandling(error)));
     });
 };
