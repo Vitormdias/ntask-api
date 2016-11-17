961317fdd8ea778c84ecc8d81177efd677073d57
vitormdias <vitormd97@gmail.com>
2016-11-17 10:17:08 -0200

    ENV config modified

---
+++ libs/config.test.js
@@ -0,0 +1,17 @@
+module.exports = {
+  database: "ntask",
+  username: "",
+  password: "",
+  params: {
+    dialect: "sqlite",
+    storage: "ntask.sqlite",
+    logging: false,
+    define: {
+      underscored: true
+    }
+  },
+  jwtSecret: "Nta$K-AP1",
+  jwtSession: {
+    session: false
+  }
+};
