70451f4ca89008df3de29499a5ea6357c4b9e918
vitormdias <vitormd97@gmail.com>
2016-11-23 11:09:27 -0200

    Config updated

--- libs/config.js
+++ libs/config.js
@@ -1,8 +1,8 @@
 module.exports = app => {
-    const env = process.env.NODE_ENV;
+  const env = process.env.NODE_ENV;
 
-    if(env) {
-        return require('./config.${env}.js');
-    }
-    return require('./config.development.js');
+  if (env) {
+    return require(`./config.${env}.js`);
+  }
+  return require("./config.development.js");
 };
