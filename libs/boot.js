0f5a3ce3afde623ef2553606f0365940db543819
vitormdias <vitormd97@gmail.com>
2016-11-24 15:44:00 -0200

    https now been used

--- libs/boot.js
+++ libs/boot.js
@@ -1,8 +1,16 @@
+import https from "https";
+import fs from "fs";
+
 module.exports = app => {
     if (process.env.NODE_ENV !== 'test') {
+        const credentials = {
+            key: fs.readFileSync("52923702-ntask-api.key", "utf8"),
+            cert: fs.readFileSync("52923702-ntask-api.cert", "utf8")
+        }
         app.db.sequelize.sync().done(() => {
-            app.listen(app.get("port"), () => {
-                console.log(`NTask API - porta ${app.get("port")}`);
+            https.createServer(credentials , app)
+                .listen(app.get("port"), () => {
+                    console.log(`NTask API - porta ${app.get("port")}`);
             });
         });
     }
