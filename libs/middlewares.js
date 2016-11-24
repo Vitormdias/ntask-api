93663f85a44c8af98e7cd8c321957d823851a34e
vitormdias <vitormd97@gmail.com>
2016-11-24 15:51:02 -0200

    Helmet implemented

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import helmet from "helmet";
 
 module.exports = app => {
   app.set("port", 3000);
@@ -7,12 +8,13 @@ module.exports = app => {
 
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
+  app.use(helmet());
   app.use(cors({
     origin: ["http://localhost:3001"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
   }));
-  
+
   app.use((req, res, next) => {
     delete req.body.id;
     next();
