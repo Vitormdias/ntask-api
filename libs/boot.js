0c70553d76b9b687eecdc97a8b93fbe4c3e5bda5
vitormdias <vitormd97@gmail.com>
2016-11-24 16:14:12 -0200

    Cors removed

--- libs/boot.js
+++ libs/boot.js
@@ -1,15 +1,7 @@
-import https from "https";
-import fs from "fs";
-
 module.exports = app => {
     if (process.env.NODE_ENV !== 'test') {
-        const credentials = {
-            key: fs.readFileSync("52923702-ntask-api.key", "utf8"),
-            cert: fs.readFileSync("52923702-ntask-api.cert", "utf8")
-        }
         app.db.sequelize.sync().done(() => {
-            https.createServer(credentials , app)
-                .listen(app.get("port"), () => {
+            app.listen(app.get("port"), () => {
                     console.log(`NTask API - porta ${app.get("port")}`);
             });
         });
--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,5 +1,4 @@
 import bodyParser from "body-parser";
-import cors from "cors";
 import helmet from "helmet";
 
 module.exports = app => {
@@ -9,11 +8,6 @@ module.exports = app => {
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use(helmet());
-  app.use(cors({
-    origin: ["http://localhost:3001"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }));
 
   app.use((req, res, next) => {
     delete req.body.id;
